f3d0120b854517b4faa616627d50b0e87d9c1429
andri191022 <andri1022@gmail.com>
2023-06-18 12:44:31 +0700

    s
    s

---
+++ Mango/Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,136 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Model;
+using Mango.Services.CouponAPI.Model.Dto;
+//using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private IMapper _mapper;
+        private ResponseDto _respose;
+        public CouponAPIController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+            _respose = new ResponseDto();
+            _respose.IsSuccess = true;
+
+        }
+
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                _respose.Result = _mapper.Map<IEnumerable<CouponDto>>(objList);
+                _respose.IsSuccess = true;
+            }
+            catch (Exception er)
+            {
+                _respose.Messages = er.Message;
+                _respose.IsSuccess = false;
+
+            }
+            return _respose;
+        }
+
+        [HttpGet]
+        [Route("GetByCode/{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(i => i.CouponId == id);
+
+
+                //CouponDto couponDto = new CouponDto()
+                //{
+                //    CouponId = obj.CouponId,
+                //    CouponCode = obj.CouponCode,
+                //    DiscountAmount = obj.DiscountAmount,
+                //    MinAmount = obj.MinAmount
+                //};
+
+                _respose.Result = _mapper.Map<CouponDto>(obj);
+                _respose.IsSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                _respose.IsSuccess = false;
+                _respose.Messages = ex.Message;
+            }
+
+            return _respose;
+        }
+
+        [HttpGet]
+        [Route("GetByCode/{code}")]
+        public ResponseDto GetByCode(string code)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(x => x.CouponCode.ToLower() == code.ToLower());
+
+                _respose.Result = _mapper.Map<CouponDto>(obj);
+                _respose.IsSuccess = true;
+
+            }
+            catch (Exception ex)
+            {
+                _respose.IsSuccess = false;
+                _respose.Messages = ex.Message;
+
+            }
+            return _respose;
+        }
+
+        [HttpPost]
+        public ResponseDto Post([FromBody] CouponDto coupondto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(coupondto);
+                _db.Coupons.Add(obj);
+                _db.SaveChanges();
+
+                _respose.IsSuccess = true;
+                _respose.Result = _mapper.Map<CouponDto>(obj);
+
+            }
+            catch (Exception ex)
+            {
+                _respose.IsSuccess = false;
+                _respose.Messages = ex.Message;
+
+            }
+            return _respose;
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(x => x.CouponId == id);
+                _db.Coupons.Remove(obj);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _respose.IsSuccess = false;
+                _respose.Messages = ex.Message;
+            }
+            return _respose;
+        }
+    }
+}
---
+++ Mango/Mango.Web/Controllers/CouponController.cs
@@ -0,0 +1,77 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCaching;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto>? list = new();
+            ResponseDto? response = await _couponService.GetAllCouponAsync();
+            if (response != null)
+            {
+                //string? rspn = response.Result == null ? "" : response.Result.ToString();
+
+                list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+            }
+
+            return View(list);
+        }
+
+        public  IActionResult CouponCreate()
+        {
+            CouponDto obj = new CouponDto();
+            return View(obj);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _couponService.CreateCouponAsync(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+            return View(model);
+        }
+
+       public async Task<IActionResult> CouponDelete(int couponId)
+        {
+            ResponseDto? response = await _couponService.GetCouponByIdAsync(couponId);
+
+            if (response != null && response.IsSuccess)
+            {
+                CouponDto? model = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponDelete(CouponDto couponDto)
+        {
+            ResponseDto? response = await _couponService.DeleteCouponAsync(couponDto.CouponId);
+
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(CouponIndex));
+            }
+
+            return View(couponDto);
+        }
+    }
+}
