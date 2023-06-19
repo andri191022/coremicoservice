9a92bc1ede11704d13c90e3b6e4e5ac404fcdf9c
andri191022 <84828445+andri191022@users.noreply.github.com>
2023-06-19 08:37:30 +0700

    d

--- Mango/Mango.Services.AuthAPI/obj/Debug/net8.0/Mango.Services.AuthAPI.AssemblyInfo.cs
+++ Mango/Mango.Services.AuthAPI/obj/Debug/net8.0/Mango.Services.AuthAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.AuthAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ef69f2afc10f5be19b195ec8a75504b36bd4f26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.AuthAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.AuthAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
